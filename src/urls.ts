e9a6b0a56dfb4acc078ee7210b075c3875edd890
odenigbogodfrey@yahoo.com <odenigbogodfrey@yahoo.com>
2022-02-20 20:32:09 +0100

    updates

--- src/urls.ts
+++ src/urls.ts
@@ -35,7 +35,7 @@ export const urls = {
     ...{
         // baseUrl: "http://nolly19o-001-site10.itempurl.com/api/",
         // baseUrl: "http://localhost:8010/proxy/api/",
-        baseUrl: "https://kwlc-web.herokuapp.com/",
+        baseUrl: "https://kwlc-web.herokuapp.com/api/",
         v1: "v1/",
         "login": "User/Login/Login",
     },
